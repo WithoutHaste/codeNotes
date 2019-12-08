75c098feedddf063bd33b8c9ea0b1b6d8b5f2470
J M <wohaste@gmail.com>
2019-12-08 08:20:50 -0700

    code notes

---
+++ microservices.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "microservices";
+	$sectionHeader = "Microservice Architecture";
+	$syntaxLanguage = "csharp";
+
+	include "codeNotesSection.php";
+?>
