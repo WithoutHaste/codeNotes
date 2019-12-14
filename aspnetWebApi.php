408aea12d494612a530b29f5dda50813cd14a9f4
J M <wohaste@gmail.com>
2019-12-14 07:50:06 -0700

    add file extensions

---
+++ aspnetWebApi.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "aspnetWebApi";
+	$sectionHeader = "ASP.Net Web API";
+	$syntaxLanguage = "csharp";
+
+	include "codeNotesSection.php";
+?>
---
+++ yaml.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "yaml";
+	$sectionHeader = "YAML";
+	$syntaxLanguage = "yaml";
+
+	include "codeNotesSection.php";
+?>
