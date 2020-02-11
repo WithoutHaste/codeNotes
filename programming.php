3e48d397bca6192523319c6b3edf38a9f801af1e
J M <wohaste@gmail.com>
2020-02-11 07:34:56 -0700

    notes

---
+++ programming.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "programming";
+	$sectionHeader = "Programming";
+	$syntaxLanguage = "csharp";
+
+	include "codeNotesSection.php";
+?>
---
+++ softwareArchitecture.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "softwareArchitecture";
+	$sectionHeader = "Software Architecture";
+	$syntaxLanguage = "csharp";
+
+	include "codeNotesSection.php";
+?>
