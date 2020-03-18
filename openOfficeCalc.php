0aab937b05742b3be40d60283d7b25db8dee89dc
J M <wohaste@gmail.com>
2020-03-18 15:35:15 -0600

    for website

---
+++ openOfficeCalc.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "openOfficeCalc";
+	$sectionHeader = "OpenOffice Calc";
+	$syntaxLanguage = "visual basic";
+
+	include "codeNotesSection.php";
+?>
---
+++ postgres.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "postgres";
+	$sectionHeader = "Postgres";
+	$syntaxLanguage = "postgres";
+
+	include "codeNotesSection.php";
+?>
