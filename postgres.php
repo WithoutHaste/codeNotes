6dde1c944f314ea45b47e243704f44d38727960f
J M <wohaste@gmail.com>
2021-02-28 10:30:18 -0700

    collapse postgres notes - website updated

--- postgres.php
+++ postgres.php
@@ -2,7 +2,7 @@
 	//ini_set('display_errors', 1);
 	
 	$section = "postgres";
-	$sectionHeader = "Postgres";
+	$sectionHeader = "Postgres, PostgreSQL, PSQL";
 	$syntaxLanguage = "postgres";
 
 	include "codeNotesSection.php";
--- psql.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//ini_set('display_errors', 1);
-	
-	$section = "psql";
-	$sectionHeader = "PostgreSQL";
-	$syntaxLanguage = "sql";
-
-	include "codeNotesSection.php";
-?>
