514b248a9c12ba3646482285b7072cbb4bb82460
J M <wohaste@gmail.com>
2021-04-10 10:28:35 -0600

    site updated

---
+++ arabicPad.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "arabicPad";
+	$sectionHeader = "Arabic Pad";
+	$syntaxLanguage = "xml";
+
+	include "codeNotesSection.php";
+?>
---
+++ managingWork.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "managingWork";
+	$sectionHeader = "Managing Work";
+	$syntaxLanguage = "json";
+
+	include "codeNotesSection.php";
+?>
