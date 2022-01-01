567a2edbc18379b62ee150df825bde1c85b5dd96
J M <wohaste@gmail.com>
2022-01-01 11:28:54 -0800

    new sections; website up to date

---
+++ aws.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "aws";
+	$sectionHeader = "AWS";
+	$syntaxLanguage = "xml";
+
+	include "codeNotesSection.php";
+?>
---
+++ winscp.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "winscp";
+	$sectionHeader = "WinSCP";
+	$syntaxLanguage = "bash";
+
+	include "codeNotesSection.php";
+?>
