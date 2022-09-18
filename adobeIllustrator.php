e6ebff7238c65b06792f0f6d9d2782c45b61c8f3
J M <wohaste@gmail.com>
2022-09-18 08:51:13 -0700

    website up to date

---
+++ adobeIllustrator.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "adobeIllustrator";
+	$sectionHeader = "Adobe Illustrator";
+	$syntaxLanguage = "text";
+
+	include "codeNotesSection.php";
+?>
---
+++ golang.php
@@ -0,0 +1,9 @@
+<?php
+	//ini_set('display_errors', 1);
+	
+	$section = "golang";
+	$sectionHeader = "Go";
+	$syntaxLanguage = "go";
+
+	include "codeNotesSection.php";
+?>
