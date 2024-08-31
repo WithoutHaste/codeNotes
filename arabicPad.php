c31ec0e24188cef4b90c576265806f4abed4bbf1
J M <wohaste@gmail.com>
2024-08-31 16:25:55 -0700

    arabicpad

--- arabicPad.php
+++ arabicPad.php
@@ -1,9 +1,114 @@
 <?php
-	//ini_set('display_errors', 1);
-	
-	$section = "arabicPad";
-	$sectionHeader = "Arabic Pad";
-	$syntaxLanguage = "xml";
-
-	include "codeNotesSection.php";
+	//switched to just HTML to style for legibility
+	//kept the url since google already indexed it, actually coming up in searches
 ?>
+
+<html>
+	<head>
+		<title>Without Haste: Arabic Pad Notes</title>
+		<meta name="description" content="Programming Notes: Arabic Pad"/>
+		<link rel="stylesheet" type="text/css" href="codeNotesSection.css" />
+		<style type="text/css">
+			div.card {
+				display: inline-block;
+				border: 1px solid gray;
+				text-align: center;
+				color: black;
+				margin: 0.25em;
+			}
+			div.arabic {
+				font-size: 28pt;
+				background-color: white;
+				padding-left: 0.5em;
+				padding-right: 0.5em;
+				padding-top: 0.25em;
+			}
+			div.diacritical {
+				font-size: 38pt;
+			}
+			div.key {
+				font-size: 14pt;
+				background-color: lightcyan;
+				padding-left: 0.5em;
+				padding-right: 0.5em;
+				padding-bottom: 0.25em;
+			}
+			div.name {
+				font-size: 10pt;
+				background-color: lightcyan;
+				font-style: italic;
+				padding-left: 0.5em;
+				padding-right: 0.5em;
+			}
+		</style>
+	</head>
+	<body>
+		<div class='fixedHeader'>
+			<div id="pageHeader">
+				<a href='http://www.withouthaste.com' class='plain'>Without Haste</a><br/>
+				<title>Arabic Pad Notes</title>
+			</div>
+		</div>
+		<div class='mainContent'>
+			<div>
+				<div class='card'><div class='arabic'>ا</div><div class='key'>a</div></div>
+				<div class='card'><div class='arabic'>ب</div><div class='key'>b</div></div>
+				<div class='card'><div class='arabic'>ت</div><div class='key'>t</div></div>
+				<div class='card'><div class='arabic'>ث</div><div class='key'>Z</div></div>
+				<div class='card'><div class='arabic'>ج</div><div class='key'>j</div></div>
+				<div class='card'><div class='arabic'>ح</div><div class='key'>h</div></div>
+				<div class='card'><div class='arabic'>خ</div><div class='key'>K</div></div>
+				<div class='card'><div class='arabic'>د</div><div class='key'>d</div></div>
+				<div class='card'><div class='arabic'>ذ</div><div class='key'>D</div></div>
+				<div class='card'><div class='arabic'>ر</div><div class='key'>r</div></div>
+				<div class='card'><div class='arabic'>ز</div><div class='key'>z</div></div>
+				<div class='card'><div class='arabic'>س</div><div class='key'>s</div></div>
+				<div class='card'><div class='arabic'>ش</div><div class='key'>S</div></div>
+				<div class='card'><div class='arabic'>ص</div><div class='key'>c</div></div>
+				<div class='card'><div class='arabic'>ض</div><div class='key'>C</div></div>
+				<div class='card'><div class='arabic'>ط</div><div class='key'>p</div></div>
+				<div class='card'><div class='arabic'>ظ</div><div class='key'>P</div></div>
+				<div class='card'><div class='arabic'>ع</div><div class='key'>'</div></div>
+				<div class='card'><div class='arabic'>غ</div><div class='key'>g</div></div>
+				<div class='card'><div class='arabic'>ف</div><div class='key'>f</div></div>
+				<div class='card'><div class='arabic'>ق</div><div class='key'>q</div></div>
+				<div class='card'><div class='arabic'>ك</div><div class='key'>k</div></div>
+				<div class='card'><div class='arabic'>ل</div><div class='key'>l</div></div>
+				<div class='card'><div class='arabic'>م</div><div class='key'>m</div></div>
+				<div class='card'><div class='arabic'>ن</div><div class='key'>n</div></div>
+				<div class='card'><div class='arabic'>ه</div><div class='key'>H</div></div>
+				<div class='card'><div class='arabic'>و</div><div class='key'>w</div></div>
+				<div class='card'><div class='arabic'>ي</div><div class='key'>y</div></div>
+			</div>
+			<hr/>
+			<div>
+				<div class='card'><div class='arabic'>ء</div><div class='key'>x</div></div>
+				<div class='card'><div class='arabic'>ة</div><div class='key'>T</div></div>
+				<div class='card'><div class='arabic'>ڤ</div><div class='key'>F</div></div>
+				<div class='card'><div class='arabic'>ڠ</div><div class='key'>G</div></div>
+				<div class='card'><div class='arabic'>چ</div><div class='key'>J</div></div>
+				<div class='card'><div class='arabic'>ڽ</div><div class='key'>N</div></div>
+				<div class='card'><div class='arabic'>ئ</div><div class='key'>O</div></div>
+				<div class='card'><div class='arabic'>ۏ</div><div class='key'>V</div></div>
+				<div class='card'><div class='arabic'>ؤ</div><div class='key'>W</div></div>
+				<div class='card'><div class='arabic'>ڬ</div><div class='key'>X</div></div>
+				<div class='card'><div class='arabic'>ى</div><div class='key'>Y</div></div>
+			</div>
+			<hr/>
+			Diacritical Marks - added to the preceding letter
+			<div>
+				<div class='card'><div class='arabic diacritical'>ﺎُ</div><div class='key'>u</div><div class='name'>dhomma</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎٌ</div><div class='key'>uu</div><div class='name'>dhammatan</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎٗ</div><div class='key'>U</div><div class='name'>inverted dhomma</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎَ</div><div class='key'>e</div><div class='name'>fathha</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎً</div><div class='key'>ee</div><div class='name'>fathatan</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎِ</div><div class='key'>i</div><div class='name'>kasra</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎٍ</div><div class='key'>ii</div><div class='name'>kasratan</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎْ</div><div class='key'>o</div><div class='name'>sukun</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎّ</div><div class='key'>v</div><div class='name'>shadda</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎٔ</div><div class='key'>&lt;</div><div class='name'>hazma above</div></div>
+				<div class='card'><div class='arabic diacritical'>ﺎٕ</div><div class='key'>&gt;</div><div class='name'>hazma below</div></div>
+			</div>
+		</div>
+	</body>
+</html>
